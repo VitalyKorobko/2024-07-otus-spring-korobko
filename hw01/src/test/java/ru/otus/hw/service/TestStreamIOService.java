c75f75d3be217f33b356ee9deff5ed2b10865370
Korobko Vitalii <korobkovn83@gmail.com>
2024-08-15 16:51:57 +0300

    added tests for StreamIOService

---
+++ hw01/src/test/java/ru/otus/hw/service/TestStreamIOService.java
@@ -0,0 +1,37 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class TestStreamIOService {
+    private IOService ioService;
+    private ByteArrayOutputStream outputStream;
+    private PrintStream printStream;
+
+
+    @BeforeEach
+    void setUp() {
+        outputStream = new ByteArrayOutputStream(10000);
+        printStream = new PrintStream(outputStream);
+        ioService = new StreamsIOService(printStream);
+    }
+
+    @Test
+    void testPrintLine() {
+        ioService.printLine("testing printLine method");
+        printStream.flush();
+        assertThat(outputStream.toString()).isEqualTo("testing printLine method\r\n");
+    }
+
+    @Test
+    void testPrintFormattedLine() {
+        ioService.printFormattedLine("testing%nprintFormattedLine method");
+        printStream.flush();
+        assertThat(outputStream.toString()).isEqualTo("testing\r\nprintFormattedLine method\r\n");
+    }
+
+}
