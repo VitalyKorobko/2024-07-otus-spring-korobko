372dad7d0b21b0f9cee48521d9e48075a9d88e4d
Korobko Vitalii <korobkovn83@gmail.com>
2024-09-16 11:57:22 +0300

    removed unused import in TestServiceImplTest

--- hw04/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
+++ hw04/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import ru.otus.hw.dao.CsvQuestionDao;
 import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Question;
