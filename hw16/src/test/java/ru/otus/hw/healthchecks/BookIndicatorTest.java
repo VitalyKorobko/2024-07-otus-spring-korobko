eec62db6fd6d9ec8bc5c9e0dee5841c16a223910
Korobko Vitalii <korobkovn83@gmail.com>
2024-12-24 22:54:06 +0300

    health test was added

--- hw16/src/test/java/ru/otus/hw/healthchecks/BookIndicatorTest.java
+++ hw16/src/test/java/ru/otus/hw/healthchecks/BookIndicatorTest.java
@@ -1,11 +1,12 @@
 package ru.otus.hw.healthchecks;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.otus.hw.dto.BookDto;
 import ru.otus.hw.services.BookService;
@@ -15,13 +16,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest({BookIndicator.class})
-@DisplayName("Health интдикатор ")
+@AutoConfigureMockMvc
+@SpringBootTest
+@DisplayName("Health индикатор ")
 public class BookIndicatorTest {
     private static final String POSITIVE_MESSAGE_TEMPLATE = "Количество книг %d";
 
@@ -30,25 +31,25 @@ public class BookIndicatorTest {
     @Autowired
     private MockMvc mvc;
 
-    @Autowired
-    private ObjectMapper mapper;
-
     @MockBean
     private BookService service;
 
+    @Autowired
+    ApplicationContext context;
+
     @Test
     @DisplayName(" должен возвращать ожидаемое сообщение, если количество книг равно 0")
-    void shouldReturnExpectedValueIfBookAmountEqual0 () throws Exception {
+    void shouldReturnExpectedValueIfBookAmountEqual0() throws Exception {
         given(service.findAll()).willReturn(new ArrayList<>());
         var result = mvc.perform(get("/monitor/health/bookIndicator"))
-                .andExpect(status().isOk())
+                .andExpect(status().is(503))
                 .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
         assertThat(result).contains(NEGATIVE_MESSAGE);
     }
 
     @Test
     @DisplayName(" должен возвращать сообщение с количеством книг")
-    void shouldReturnExpectedValueIfBookAmountNotEqual0 () throws Exception {
+    void shouldReturnExpectedValueIfBookAmountNotEqual0() throws Exception {
         List<BookDto> list = List.of(new BookDto(1L, "", null, null));
         given(service.findAll()).willReturn(list);
         var result = mvc.perform(get("/monitor/health/bookIndicator"))
